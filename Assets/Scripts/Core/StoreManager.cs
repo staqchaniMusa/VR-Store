58ffbdd557f15dfe79dc15fe15b16c9208721a31
staqchaniMusa <musaalistaqchani@gmail.com>
2022-08-02 10:23:12 -0700

    apk updated

--- Assets/Scripts/Core/StoreManager.cs
+++ Assets/Scripts/Core/StoreManager.cs
@@ -7,7 +7,7 @@ public class StoreManager : MonoBehaviour
     public Transform ActivePlayer;
     public Transform Canvas;
     public static StoreManager Instance;
-
+    private float yOffset;
     private void Awake()
     {
         if (Instance == null)
@@ -16,20 +16,33 @@ public class StoreManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
         else DestroyImmediate(gameObject);
+        yOffset = Canvas.transform.position.y;
+        Invoke(nameof(ResetCanvas), 2f);
     }
 
     private void Update()
     {
         if(InputBridge.Instance.AButtonDown || InputBridge.Instance.XButtonDown)
         {
+
             if (Canvas.gameObject.activeInHierarchy)
             {
+               /* float dot = Vector3.Dot(ActivePlayer.transform.position, Canvas.transform.position);
+                if (dot > 0.3f)
+                    ResetCanvas();
+                else*/
                 Canvas.gameObject.SetActive(false);
             }else
             {
-                Canvas.gameObject.SetActive(true);
-                Canvas.transform.position = ActivePlayer.transform.position + ActivePlayer.transform.forward * 1f;
+                ResetCanvas();
             }
         }
+    }
+
+    void ResetCanvas()
+    {
+        Canvas.gameObject.SetActive(true);
+        Canvas.transform.position = ActivePlayer.transform.position + ActivePlayer.transform.forward * 1f + Vector3.up * yOffset;
+        Canvas.transform.forward = ActivePlayer.transform.forward;
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # VR Store
  A basic store front UI to purchase things
+
+ # Features
+ there are three categories
+ - Furniture
+ - Weapon
+ - Sports
+
+ # How to Use
+ some furniture are only interactable like Door which you can close and open by holding its handle. some are interactable like all weapons. 
+ press Grip to grab the object either from remote or by physically grab the object.
+ to use weapon you have to separately buy bullet clip and load as well.
+
+ press A or x button to reset or toggle your the canvas.
